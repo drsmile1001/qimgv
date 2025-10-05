ed090b19640d0f80ea8ae59b56fbcc67e82c4401
drsmile1001 <drsmile.1001@gmail.com>
2025-10-06 07:54:24 +0800

    移除隱性的方向鍵上觸發fitMode

--- qimgv/gui/viewers/viewerwidget.cpp
+++ qimgv/gui/viewers/viewerwidget.cpp
@@ -592,15 +592,6 @@ void ViewerWidget::keyPressEvent(QKeyEvent *event) {
         videoPlayer->pauseResume();
         return;
     }
-    if(currentWidget == IMAGEVIEWER && imageViewer->isDisplaying()) {
-        // switch to fitWidth via up arrow
-        if(ShortcutBuilder::fromEvent(event) == "Up" && !actionManager->actionForShortcut("Up").isEmpty()) {
-            if(imageViewer->fitMode() == FIT_WINDOW && imageViewer->scaledImageFits()) {
-                imageViewer->setFitWidth();
-                return;
-            }
-        }
-    }
     actionManager->processEvent(event);
 }
 
